a8b1dce6a944b1338e121a925a67b6c753a17138
krahul2024 <culerrahul2024@gmail.com>
2025-06-24 11:19:45 +0530

    a bit of dart

--- ch-one/main.dart
+++ ch-one/main.dart
@@ -1,3 +1,29 @@
-void main() {
-    print("Hello, World");
+void main() { // this can also be main(List<String> args)
+    String? name = 'my name';
+    print(name);
+
+    isPositive(0);
+
+    paramFunction();
+    // paramFunction(true, 0); // not supported
+    paramFunction(b: true);
 }
+
+bool isPositive(int x) {
+    return x > 0;
+}
+
+bool isNegative(int x) =>  x < 0;
+
+// to have default values should be enclosed in {  },
+// and should have 'type? x' and not 'type x'
+// as per dart when enclosed it should default to null or something of sorts
+// Dart has runes similar to go, basically unicodes -> int
+
+void paramFunction({bool? b, int? x, String s = 'some value'}) {
+
+}
+
+// Uninitialized variables that have a nullable type have an initial value of null.
+// Even variables with numeric types are initially null, because numbers—like everything
+// else in Dart—are objects.
