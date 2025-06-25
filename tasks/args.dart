afa38a32b5e5bcd31068846e480c01435a8276e4
krahul2024 <culerrahul2024@gmail.com>
2025-06-26 01:07:30 +0530

    able parse commands

--- tasks/args.dart
+++ tasks/args.dart
@@ -1,42 +1,90 @@
 class Arg {
-    final String short;
-    final String long;
-    final String description;
+  final String short;
+  final String long;
+  final String description;
 
-    // make every object immutable
-    const Arg(this.short, this.long, this.description);
+  // make every object immutable
+  const Arg(this.short, this.long, this.description);
 
-    void show() {
-        final shortPadded = short.padRight(1);
-        final longPadded = long.padRight(10);
+  void show() {
+    final shortPadded = short.padRight(1);
+    final longPadded = long.padRight(10);
 
-        print("    -${shortPadded}  --${longPadded}  ${description}");
-    }
+    print("    -${shortPadded}  --${longPadded}  ${description}");
+  }
 }
 
 abstract class Args {
-    static final usageMsg = '''
+  static final usageMsg = '''
 Task CLI – manage your to-do list from the terminal
 
 Usage:
   dart run main.dart [command] [options]
 
-Options:''';
-    static final List<Arg> args = [
-        const Arg('a', 'add', 'Add a new task'),
-        const Arg('d', 'done', 'Mark a task as done'),
-        const Arg('r', 'remove', 'Remove a task'),
-        const Arg('l', 'list', 'List all tasks'),
-        helpArg,
-    ];
+Commands:''';
+  static final List<Arg> args = [
+    const Arg('a', 'add', 'Add a new task'),
+    const Arg('d', 'done', 'Mark a task as done'),
+    const Arg('r', 'remove', 'Remove a task'),
+    const Arg('l', 'list', 'List all tasks'),
+    helpArg,
+  ];
 
-    static void printUsage() {
-        print(usageMsg);
+  static void printUsage() {
+    print(usageMsg);
 
-        for (Arg arg in args) {
-            arg.show();
-        }
+    for (Arg arg in args) {
+      arg.show();
     }
+  }
+
+  static String validateFlag(String flagArg) {
+    if (flagArg.length == 2) {
+      flagArg = flagArg.replaceFirst('-', '');
+    } else if (flagArg.length > 2) {
+      flagArg = flagArg.replaceFirst('--', '');
+    }
+
+    return flagArg;
+  }
+
+  static ArgAction matchArg(String flagArg) {
+    String flag = validateFlag(flagArg);
+    ArgAction flagAction;
+
+    switch (flag) {
+      case 'a':
+        flagAction = ArgAction.ADD;
+        break;
+      case 'add':
+        flagAction = ArgAction.ADD;
+        break;
+
+      case 'd':
+        flagAction = ArgAction.DONE;
+        break;
+      case 'done':
+        flagAction = ArgAction.DONE;
+        break;
+
+      case 'r':
+        flagAction = ArgAction.REMOVE;
+      case 'remove':
+        flagAction = ArgAction.REMOVE;
+
+      case 'l':
+        flagAction = ArgAction.LIST;
+      case 'list':
+        flagAction = ArgAction.LIST;
+
+      default:
+        flagAction = ArgAction.NONE;
+    }
+
+    return flagAction;
+  }
 }
+
+enum ArgAction { ADD, DONE, REMOVE, LIST, NONE }
 
 const Arg helpArg = Arg('h', 'help', 'Usage help');
--- tasks/main.dart
+++ tasks/main.dart
@@ -1,5 +1,14 @@
 import 'args.dart';
 
 void main(List<String> args) {
+  try {
+    if (args.isEmpty) throw ArgumentError('No arguments provided');
+
+    ArgAction action = Args.matchArg(args[0]);
+    if (action == ArgAction.NONE)
+      throw ArgumentError('Unknown Flag: ${args[0]}');
+  } catch (e) {
+    print('Error: $e\n');
     Args.printUsage();
+  }
 }
